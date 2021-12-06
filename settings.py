980289893f475a773d7501e85cfd42511457bbdb
eyal1izhaki <eyal1izhaki@gmail.com>
2021-12-06 10:31:11 +0200

    Fruit now gets a random position in it constructor

--- settings.py
+++ settings.py
@@ -12,7 +12,7 @@ BORDER_COLOR = (49, 39, 18)
 BORDER_THICKNESS = 2 # Number of cells
 
 # Delay between screen update to another. Snakes move one cell every screen update. 0.07 looks good
-DELAY_BETWEEN_SCREEN_UPDATES = 0.2 # second
+DELAY_BETWEEN_SCREEN_UPDATES = 0.07 # second
 
 STARTING_SNAKE_SIZE = 3
 
