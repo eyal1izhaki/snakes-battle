34b87e4e09ff60c71f6a1770a4f6ebf22a4d8d25
alphabestteam <alphabestteam@gmail.com>
2021-12-15 18:53:42 +0200

    snake_win

--- my_playground.py
+++ my_playground.py
@@ -2,16 +2,17 @@
 #  I test the code here so the run.py file stays clean. #
 #########################################################
 
+from math import trunc
 import pygame
 import sys
 from time import sleep
-
+import time
 
 from snakes_battle.board import Board
 from snakes_battle.fruits.StrawberryFruit import StrawberryFruit
 from snakes_battle.ai_snake import AISnake
 from snakes_battle import graphics,rules
-
+from tkinter import messagebox
 import settings
 
 pygame.init()
@@ -23,15 +24,15 @@ board = Board()
 snake_a = AISnake(board.border_cells)
 snake_b = AISnake(board.border_cells)
 
-snakes_array = [snake_a]
+snakes_array = [snake_a,snake_b]
 
 for snake in snakes_array:
     board.add_snake(snake)
 
 board.add_fruit(StrawberryFruit(rules.get_new_fruit_position(board)))
+time_pass=time.time()
 
-while True:
-
+while (time.time() - time_pass <= 60 and len(board.snakes) > 0):
     sleep(settings.DELAY_BETWEEN_SCREEN_UPDATES)
 
     for event in pygame.event.get():
@@ -52,4 +53,14 @@ while True:
     graphics.update_screen(surface, board)
 
     rules.apply_rules(board)
-    
+
+combined_snakes = board.snakes + board.lost_snakes
+snakes_win = [combined_snakes[0]]
+
+for snake in combined_snakes[1:]:
+    if snakes_win[0].length==snake.length:
+        snakes_win.append(snake)
+    elif snakes_win[0].length<snake.length:
+        snakes_win = [snake]
+
+messagebox.showinfo("winner",snakes_win[0].name+" is the winner!!!")
--- snakes_battle/ai_snake.py
+++ snakes_battle/ai_snake.py
@@ -1,9 +1,10 @@
 from snakes_battle.snake import Snake, Direction
 
 class AISnake(Snake):
-    def __init__(self, borders_cells) -> None:
+    def __init__(self, borders_cells,name="test") -> None:
         super().__init__()
-
+        #player name
+        self.name = name
         # All the cells that are fill with borders. This variable will store a list of (x, y) pairs
         self.border_cells = borders_cells
 
