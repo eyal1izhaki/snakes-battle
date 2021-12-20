404680a43be70e9041204d345a0c7419dc859c0a
eyal1izhaki <eyal1izhaki@gmail.com>
2021-12-20 15:09:25 +0200

    unique and random head position function moved to rules module.

--- snakes_battle/rules.py
+++ snakes_battle/rules.py
@@ -3,15 +3,15 @@
 # For example, If snake eats a fruit, we grows. If a snake hit a border a loses and so on...
 
 import random
-import sys
-import time
+import math
 import settings
+from snakes_battle.board import Board
 from snakes_battle.fruits.StrawberryFruit import StrawberryFruit
 from snakes_battle.fruits.DragonFruit import DragonFruit
 from snakes_battle.fruits.Bomb import Bomb
 
 harmfulFruits = (Bomb, ) # List of fruits that should not create new fruits to the board
-benefitialFruits = (StrawberryFruit, DragonFruit)
+beneficialFruits = (StrawberryFruit, DragonFruit)
 
 def apply_rules(board):
     for fruit in board.fruits:
@@ -30,20 +30,20 @@ def apply_rules(board):
                     snake_lost(snake,board)
 
                 if (not isinstance(fruit, harmfulFruits)):
-                    new_fruit_type = random.choice(benefitialFruits)
+                    new_fruit_type = random.choice(beneficialFruits)
                     board.add_fruit(new_fruit_type(get_new_fruit_position(board)))
 
         # Rule: Snake hitted a border
         if snake.body_pos[0][0] == settings.BORDER_THICKNESS-1: # Hitted left border
             snake_lost(snake,board)
 
-        if snake.body_pos[0][0] == settings.BOARD_SIZE[0] - settings.BORDER_THICKNESS: # Hitted right border
+        if snake.body_pos[0][0] == board.board_size[0] - settings.BORDER_THICKNESS: # Hitted right border
             snake_lost(snake,board)
 
         if snake.body_pos[0][1] == settings.BORDER_THICKNESS-1: # Hitted upper border
             snake_lost(snake,board)
 
-        if snake.body_pos[0][1] == settings.BOARD_SIZE[1] - settings.BORDER_THICKNESS: # Hitted bottom border
+        if snake.body_pos[0][1] == board.board_size[1] - settings.BORDER_THICKNESS: # Hitted bottom border
             snake_lost(snake,board)
 
         # Rule: Snake hitted itself or other snakes
@@ -56,7 +56,7 @@ def apply_rules(board):
             elif snake.body_pos[0] in _snake.body_pos: # snake hitted other snakes
                 snake_lost(snake,board)
         
-        # Rule - generate a bomb randomlly.
+        # Rule - generate a bomb randomly.
         if (random.random() < Bomb.BOMB_CREATION_PROBABILITY):
             board.add_fruit(Bomb(get_new_fruit_position(board)))
 
@@ -69,4 +69,21 @@ def get_new_fruit_position(board):
     
     board._update_empty_cells()
     return list(random.choice(board.empty_cells))
+
+def get_unique_snake_head_position(board: Board):
+
+    # Generating random and unique position to the head of the snake, that will not collide with other snake's tail
+    generate_position = True
+    while (generate_position == True):
+        random_head_position = list(random.choice(board.all_cells_pos))
+        too_close = False
+        for snake in board.snakes:
+            if (math.dist(snake.body_pos[0], random_head_position) < settings.STARTING_SNAKE_LENGTH):
+                too_close = True
+                break
+        generate_position = too_close
+
+    return random_head_position
+
+
 
