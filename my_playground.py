0126fa1a4859d0ebe16c2ad3512e69d34d7d230b
eyal1izhaki <eyal1izhaki@gmail.com>
2021-12-20 15:13:23 +0200

    we now getting a new head position in our main module.

--- my_playground.py
+++ my_playground.py
@@ -9,28 +9,32 @@ import time
 from snakes_battle.board import Board
 from snakes_battle.fruits.StrawberryFruit import StrawberryFruit
 from snakes_battle.ai_snake import AISnake
-from snakes_battle import graphics,rules
+from snakes_battle import rules
+from snakes_battle.graphics import GameGraphics
 from tkinter import messagebox
 import settings
 
-pygame.init()
+graphics = GameGraphics()
+board = Board(graphics.board_size)
 
-surface = graphics.create_surface()
-board = Board()
-
-
-snake_a = AISnake(board.border_cells, name = "Yagel")
-snake_b = AISnake(board.border_cells, name = "David")
+snake_a = AISnake(board.border_cells, color=graphics.get_unique_snake_color(), name="Yagel")
+snake_b = AISnake(board.border_cells, color=graphics.get_unique_snake_color(), name="David")
 
 snakes_array = [snake_a, snake_b]
 
 for snake in snakes_array:
+    # The position of the head is determined by the rules and the board state.
+    head_pos = rules.get_unique_snake_head_position(board)
+    snake.body_pos = [head_pos]
+    for _ in range(settings.STARTING_SNAKE_LENGTH - 1):
+        snake._grow_in_one_unit()
+
     board.add_snake(snake)
 
 board.add_fruit(StrawberryFruit(rules.get_new_fruit_position(board)))
-time_pass=time.time()
+time_pass = time.time()
 
-while (time.time() - time_pass <= settings.GAME_TIME_LENGTH and len(board.snakes) > 0):
+while time.time() - time_pass <= settings.GAME_TIME_LENGTH and len(board.snakes) > 0:
     time.sleep(settings.DELAY_BETWEEN_SCREEN_UPDATES)
 
     for event in pygame.event.get():
@@ -42,13 +46,12 @@ while (time.time() - time_pass <= settings.GAME_TIME_LENGTH and len(board.snakes
             if event.key == pygame.K_a:
                 board.add_fruit(StrawberryFruit(rules.get_new_fruit_position(board)))
 
-
     # The AI Snake Should make a decision in which direction to go.
     for snake in snakes_array:
         snake.change_direction(board.get_board_state())
         snake.move_one_cell()
-    
-    graphics.update_screen(surface, board)
+
+    graphics.update_screen(board)
 
     rules.apply_rules(board)
 
@@ -56,9 +59,9 @@ combined_snakes = board.snakes + board.lost_snakes
 snakes_win = [combined_snakes[0]]
 
 for snake in combined_snakes[1:]:
-    if snakes_win[0].length==snake.length:
+    if snakes_win[0].length == snake.length:
         snakes_win.append(snake)
-    elif snakes_win[0].length<snake.length:
+    elif snakes_win[0].length < snake.length:
         snakes_win = [snake]
 
-messagebox.showinfo("winner",snakes_win[0].name+" is the winner!!!")
+messagebox.showinfo("winner", snakes_win[0].name + " is the winner!!!")
