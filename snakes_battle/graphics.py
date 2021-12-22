3e98a086c2ec84a146eedadc55a85777cd2fbaaa
eyal1izhaki <eyal1izhaki@gmail.com>
2021-12-22 15:55:51 +0200

    snake now little bit smaller than cell size.

--- snakes_battle/graphics.py
+++ snakes_battle/graphics.py
@@ -61,7 +61,9 @@ class GameGraphics:
             image = self._load_and_scale_image(image_path)
             self.images[name] = image
 
-        self.images["HEAD"] = self._load_and_scale_image("snakes_battle\\images\\snake_head.png", fit_width=True, fit_height=False)
+        # Loads all snakes heads
+        for color in settings.SNAKES_COLORS:
+            self.images[f"HEAD_{str(color)}"] = self._load_and_scale_image(f"snakes_battle\\images\\snake_heads\snake_head{str(color)}.png", fit_width=True, fit_height=False)
 
         self.title_font = pygame.font.SysFont('Arial Black', settings.SCOREBOARD_TITLE_FONT_SIZE)
         self.subtitle_font = pygame.font.SysFont('Arial Black', 10)
@@ -109,21 +111,21 @@ class GameGraphics:
     def _draw_snake(self, snake, margin=4):
 
         snake_head_offset_to_body = 2
-        head_image = self.images["HEAD"]
+        head_image = self.images[f"HEAD_{str(snake.color)}"]
 
 
         # Creating the turn block surface. We will rotate it as needed.
-        snake_turn_block = pygame.Surface((self.cell_size, self.cell_size))
-        snake_turn_block.set_colorkey((0, 0, 0))
-        x1 = (margin - 1, margin - 1)
-        x2 = (self.cell_size - 1, margin - 1)
-        x3 = (self.cell_size - 1, self.cell_size - margin - 1)
-        x4 = (self.cell_size - margin - 1, self.cell_size - margin - 1)
-        x5 = (self.cell_size - margin - 1 ,self.cell_size - 1)
-        x6 = (margin - 1, self.cell_size - 1)
+        snake_turn_block = pygame.Surface((self.cell_size + 1, self.cell_size + 1))
+        snake_turn_block.set_colorkey((0,0,0))
+        x1 = (margin, margin)
+        x2 = (self.cell_size, margin)
+        x3 = (self.cell_size, self.cell_size - margin)
+        x4 = (self.cell_size - margin, self.cell_size - margin)
+        x5 = (self.cell_size - margin ,self.cell_size)
+        x6 = (margin, self.cell_size)
 
+        # The turn block is in a position of: form up direction turn right.
         pygame.draw.polygon(snake_turn_block, snake.color, [x1,x2,x3,x4,x5,x6])
-
 
         # Drawing the head and the rest of the snakes body.
         head=True
@@ -159,90 +161,60 @@ class GameGraphics:
 
                 coordinates = self._get_cell_coordinates(snake.body_pos[i])
 
-                 # Checking if this block in the snake body is a turn block.
-                if i < len(snake.body_pos) - 2 and snake.body_pos[i-1][1] == snake.body_pos[i][1] != snake.body_pos[i+1][1]:
+                # Checking if this block in the snake body is a turn block.
+                if i < len(snake.body_pos) - 1 and snake.body_pos[i-1][1] != snake.body_pos[i+1][1] and snake.body_pos[i-1][0] != snake.body_pos[i+1][0]:
 
-                        # Up and left or right.
-                        if snake.body_pos[i][1] < snake.body_pos[i+1][1]:
-                            
-                            # Left turn.
-                            if snake.body_pos[i-1][0] < snake.body_pos[i+1][0]:
-                                top_right = (top_right[0] - margin, top_right[1])
-                                bottom_right = (bottom_right[0] - margin, bottom_right[1])
-                            
-                            # Right turn.
-                            if snake.body_pos[i-1][0] > snake.body_pos[i+1][0]:
-                                top_left = (top_left[0] + margin, top_left[1])
-                                bottom_left = (bottom_left[0] + margin, bottom_left[1])
-
-                            fill_top_left, fill_bottom_left, fill_bottom_right, fill_top_right = coordinates
-
-                            fill_coordinates = [
-                                (fill_bottom_left[0] + margin, fill_bottom_left[1] - margin),
-                                (fill_bottom_left[0] + margin, fill_bottom_left[1]),
-                                (fill_bottom_right[0] - margin, fill_bottom_right[1]),
-                                (fill_bottom_right[0] - margin, fill_bottom_right[1] - margin)
-                                ]
-
-                            pygame.draw.polygon(self.surface, snake.color, fill_coordinates)
+                    turn_block_top_left = coordinates[0]
+                    # Up and left or right.
+                    if snake.body_pos[i+1][1] > snake.body_pos[i][1]:
+                        # Left
+                        if snake.body_pos[i][0] > snake.body_pos[i-1][0]:
+                            self.surface.blit(pygame.transform.rotate(snake_turn_block, -90), turn_block_top_left)
                         
-                        # Down and then left of right turn.
-                        elif snake.body_pos[i][1] > snake.body_pos[i+1][1]:
-                             # Left turn.
-                            if snake.body_pos[i-1][0] < snake.body_pos[i+1][0]:
-                                top_right = (top_right[0] - margin, top_right[1])
-                                bottom_right = (bottom_right[0] - margin, bottom_right[1])
-                            
-                            # Right turn.
-                            else:
-                                top_left = (top_left[0] + margin, top_left[1])
-                                bottom_left = (bottom_left[0] + margin, bottom_left[1])
-                            
-                            fill_top_left, fill_bottom_left, fill_bottom_right, fill_top_right = coordinates
-
-                            fill_coordinates = [
-                                (fill_top_left[0] + margin, fill_top_left[1]),
-                                (fill_top_left[0] + margin, fill_top_left[1] + margin),
-                                (fill_top_right[0] - margin, fill_top_right[1] + margin),
-                                (fill_top_right[0] - margin, fill_top_right[1])
-                                ]
-
-                        # Left and the up or down.
-                        elif snake.body_pos[i][0] < snake.body_pos[i+1][0]:
-
-                            # Up.
-                            if snake.body_pos[i-1][1] < snake.body_pos[i+1][1]:
-                                bottom_left = (bottom_left[0], bottom_left[1]-margin)
-                                bottom_right = (bottom_right[0], bottom_right[1]-margin)
-
-                            # Down.
-                            else:
-                                top_left = (top_left[0], top_left[1] + margin)
-                                top_right = (top_right[0], top_right[1] + margin)
-
-                            fill_top_left, fill_bottom_left, fill_bottom_right, fill_top_right = coordinates
-
-                            fill_coordinates = [
-                                (fill_top_right[0] + margin, fill_top_left[1]),
-                                (fill_top_right[0] + margin, fill_top_left[1] + margin),
-                                (fill_top_right[0] - margin, fill_top_right[1] + margin),
-                                (fill_top_right[0] - margin, fill_top_right[1])
-                                ]
-
-                            
-                                
-                        
-                            
-
+                        # Right
+                        else:
+                            self.surface.blit(snake_turn_block, turn_block_top_left)
                     
+                    # Down and left ot right.
+                    elif snake.body_pos[i+1][1] < snake.body_pos[i][1]:
+                        # Left
+                        if snake.body_pos[i][0] > snake.body_pos[i-1][0]:
+                            self.surface.blit(pygame.transform.rotate(snake_turn_block, 180), turn_block_top_left)
+                        # Right
+                        else:
+                            self.surface.blit(pygame.transform.rotate(snake_turn_block, 90), turn_block_top_left)
+                        
+                    # Right and up or down.
+                    elif snake.body_pos[i][0] < snake.body_pos[i+1][0]:
+                        # Up
+                        if snake.body_pos[i][1] > snake.body_pos[i-1][1]:
+                            self.surface.blit(pygame.transform.rotate(snake_turn_block, 90), turn_block_top_left)
+                        # Down
+                        else:
+                            self.surface.blit(snake_turn_block, turn_block_top_left)
+
+                    # Left and up or down
+                    elif snake.body_pos[i][0] > snake.body_pos[i+1][0]:
+                        # Up
+                        if snake.body_pos[i][1] > snake.body_pos[i-1][1]:
+                            self.surface.blit(pygame.transform.rotate(snake_turn_block, 180), turn_block_top_left)
+                        # Down
+                        else:
+                            self.surface.blit(pygame.transform.rotate(snake_turn_block, -90), turn_block_top_left)
+
                 # Square is horizontal (The block is in right or left direction).
-                if  snake.body_pos[i][1] == snake.body_pos[i-1][1]:
+                elif  snake.body_pos[i][1] == snake.body_pos[i-1][1]:
 
                     top_left, bottom_left, bottom_right, top_right = coordinates
                     top_left = (top_left[0], top_left[1] + margin)
                     top_right = (top_right[0], top_right[1] + margin)
                     bottom_left = (bottom_left[0],bottom_left[1] - margin)
                     bottom_right = (bottom_right[0], bottom_right[1] - margin)
+
+                                        
+                    coordinates = [top_left, bottom_left, bottom_right, top_right]
+
+                    pygame.draw.polygon(self.surface, snake.color, coordinates)
 
                 # Square is vertical (The block is in up or down direction).
                 elif snake.body_pos[i][0] == snake.body_pos[i-1][0]:
@@ -251,11 +223,12 @@ class GameGraphics:
                     top_right = (top_right[0] - margin, top_right[1])
                     bottom_left = (bottom_left[0] + margin, bottom_left[1])
                     bottom_right = (bottom_right[0] - margin, bottom_right[1])
+
+                    coordinates = [top_left, bottom_left, bottom_right, top_right]
+
+                    pygame.draw.polygon(self.surface, snake.color, coordinates)
+
                 
-                coordinates = [top_left, bottom_left, bottom_right, top_right]
-
-                pygame.draw.polygon(self.surface, snake.color, coordinates)
-
 
     def _draw_fruit(self, fruit, draw_background=True):
         
