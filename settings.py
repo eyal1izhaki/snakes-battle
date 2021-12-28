45d4e70d3b51d2df75c9a5cf25786232499cd07a
Alpha Team <alphabestteam@gmail.com>
2021-12-28 14:50:47 +0200

    Added manual controlled snakes for human play

--- settings.py
+++ settings.py
@@ -31,7 +31,7 @@ DEAD_SNAKE_IMAGE_PATH = "snakes_battle\\images\\dead_icon.png"
 
 
 # General
-DELAY_BETWEEN_SCREEN_UPDATES = 0.04 # Delay between screen update to another. Snakes move one cell every screen update. 0.07 looks good
+DELAY_BETWEEN_SCREEN_UPDATES = 0.06 # Delay between screen update to another. Snakes move one cell every screen update. 0.07 looks good
 
 ######################################
 
---
+++ snakes_battle/snakes_ai/manual_control_snake_wasd.py
@@ -0,0 +1,21 @@
+import pygame
+from snakes_battle.snake import Snake, Direction
+
+class ManualSnakeWASD(Snake):
+    def __init__(self, borders_cells, color, name) -> None:
+        super().__init__(color, name)
+
+        # All the cells that are fill with borders. This variable will store a list of (x, y) pairs
+        self.border_cells = borders_cells
+    
+    def change_direction(self, board_state, events):
+        for event in events:
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_a:
+                    super().change_direction(Direction.LEFT)
+                elif event.key == pygame.K_d:
+                    super().change_direction(Direction.RIGHT)
+                elif event.key == pygame.K_w:
+                    super().change_direction(Direction.UP)
+                elif event.key == pygame.K_s:
+                    super().change_direction(Direction.DOWN)
