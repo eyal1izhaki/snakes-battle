8d9eaf91de1cdcf9492d43c2d091807a9714c8b4
Tehila <tehilare111@gmail.com>
2022-01-03 10:58:19 +0200

    v1.2

--- snakes_battle/snakes_ai/tehila_bot.py
+++ snakes_battle/snakes_ai/tehila_bot.py
@@ -6,16 +6,10 @@ class Tehila(Snake):
     def __init__(self, borders_cells, color, name) -> None:
         super().__init__(color, name)
         self.init(borders_cells)
-        
-
-    ##############################
-    # You can edit only the code below. You can't change methods names.
-    ##############################
-
 
     def init(self, borders_cells):
         # Your bot initializations will be here.
-        self.allowed__version = 1.1
+        self.allowed__version = 1.2
 
         # All the cells that are fill with borders. This variable will store a list of (x, y) pairs
         self.allowed__border_cells = borders_cells
@@ -42,17 +36,23 @@ class Tehila(Snake):
                     return True
         return False
 
-    def check_if_position_in_snake(self, snake_pos, direction, fruits, snakes):
-        snake_head=snake_pos[0]
-        # for snake_node in snake_pos:
+
+    def get_pos_to_check(self, direction, pos):
         if direction == Direction.UP:
-            pos_to_check = [snake_head[0] , snake_head[1]-1]
+            pos_to_check = [pos[0] , pos[1]-1]
         if direction == Direction.RIGHT:
-            pos_to_check = [snake_head[0]+1 , snake_head[1]]
+            pos_to_check = [pos[0]+1 , pos[1]]
         if direction == Direction.DOWN:
-            pos_to_check = [snake_head[0] , snake_head[1]+1]
+            pos_to_check = [pos[0] , pos[1]+1]
         if direction == Direction.LEFT:
-            pos_to_check = [snake_head[0]-1 , snake_head[1]]
+            pos_to_check = [pos[0]-1 , pos[1]]
+        return pos_to_check
+
+    def check_if_position_in_snake(self, snake_pos, direction, fruits, snakes, pos_to_check=None):
+
+        if not pos_to_check:
+            pos_to_check = snake_pos[0]
+        pos_to_check = self.get_pos_to_check(direction,pos_to_check)
         
         
         if self.is_pos_snake(pos_to_check, snake_pos):
@@ -69,15 +69,19 @@ class Tehila(Snake):
         
         return False
     
-    def allowed__change_direction(self, pos, fruit_to_follow, fruits, snakes):
+    def allowed__change_direction(self, pos, fruit_to_follow, fruits, snakes,pos_to_check):
         if pos[0][0] > fruit_to_follow.pos[0]:
             if (self.direction == Direction.RIGHT):
+                if not self.check_if_position_in_snake(pos,Direction.UP,fruits, snakes,pos_to_check):
+                    return Direction.DOWN
                 return Direction.UP
             else:
                 return Direction.LEFT
         
         if pos[0][0] < fruit_to_follow.pos[0]:
             if (self.direction == Direction.LEFT):
+                if not self.check_if_position_in_snake(pos,Direction.UP,fruits, snakes,pos_to_check):
+                    return Direction.DOWN
                 return Direction.UP
             else:
                 return Direction.RIGHT
@@ -86,12 +90,16 @@ class Tehila(Snake):
 
             if pos[0][1] <fruit_to_follow.pos[1]:
                 if (self.direction == Direction.UP):
+                    if not self.check_if_position_in_snake(pos,Direction.RIGHT,fruits, snakes):
+                        return Direction.LEFT
                     return Direction.RIGHT
                 else:
                     return Direction.DOWN
 
             if pos[0][1] > fruit_to_follow.pos[1]:
                 if (self.direction == Direction.DOWN):
+                    if not self.check_if_position_in_snake(pos,Direction.RIGHT,fruits, snakes):
+                        return Direction.LEFT
                     return Direction.RIGHT
                 else:
                     return Direction.UP
@@ -102,36 +110,9 @@ class Tehila(Snake):
         y = abs(pos_2[1] - pos_1[1])
         return x+y
     
-    
 
-    def make_decision(self, board_state):
-        # You can only call methods that starts with the word 'allowed__'. You can't change attrbiutes directly.
-
-        # super().allowed__get_direction() # This function takes no arguments and returns the direction of the snake.
-        # super().allowed__body_position() # This function takes no arguments and returns a list with all cells (x,y) that are filled with your snake.
-        # super().allowed__is_king() # returns True if your snake is king else returns False
-        # super().allowed__is_knife() # returns True if your snake has a knife else returns False.
-        # super().allowed__is_shield() # returns True if your snake is shielded else returns False.
-
-        pos = super().allowed__body_position()
-        fruits = board_state["fruits"]
-        snakes = board_state["snakes"]
-
-        # fruit_to_follow = fruits[0]
-        min_fruit_dis =  self.get_distance(pos[0], fruits[0].pos)
-        fruit_to_follow =  fruits[0]
-        for fruit in fruits: 
-            if fruit.kind not in FruitKind.harmful_fruits:
-                fruit_dis = self.get_distance(pos[0], fruit.pos)
-                if fruit_dis <= min_fruit_dis:
-                    min_fruit_dis = fruit_dis
-                    fruit_to_follow = fruit
-
-
-
-        direction = self.allowed__change_direction(pos, fruit_to_follow, fruits, snakes)
-        
-        if direction == Direction.UP and self.check_if_position_in_snake(pos,Direction.UP,fruits, snakes):
+    def get_direction_after_clear_way(self, direction, pos, fruits, snakes, pos_to_check):
+        if direction == Direction.UP and self.check_if_position_in_snake(pos,Direction.UP,fruits, snakes,pos_to_check):
             if not self.check_if_position_in_snake(pos,Direction.DOWN,fruits, snakes):
                 return Direction.DOWN
             if not self.check_if_position_in_snake(pos,Direction.LEFT,fruits, snakes):
@@ -140,7 +121,7 @@ class Tehila(Snake):
 
 
         if direction == Direction.DOWN and self.check_if_position_in_snake(pos,Direction.DOWN,fruits, snakes):
-            if not self.check_if_position_in_snake(pos,Direction.UP,fruits, snakes):
+            if not self.check_if_position_in_snake(pos,Direction.UP,fruits, snakes,pos_to_check):
                 return Direction.UP
             if not self.check_if_position_in_snake(pos,Direction.LEFT,fruits, snakes):
                 return Direction.LEFT
@@ -150,18 +131,56 @@ class Tehila(Snake):
         if direction == Direction.RIGHT and self.check_if_position_in_snake(pos,Direction.RIGHT,fruits, snakes):
             if not self.check_if_position_in_snake(pos,Direction.LEFT,fruits, snakes):
                 return Direction.LEFT
-            if not self.check_if_position_in_snake(pos,Direction.UP,fruits, snakes):
+            if not self.check_if_position_in_snake(pos,Direction.UP,fruits, snakes,pos_to_check):
                 return Direction.UP
             return Direction.DOWN
 
         if direction == Direction.LEFT and self.check_if_position_in_snake(pos,Direction.LEFT,fruits, snakes):
             if not self.check_if_position_in_snake(pos,Direction.RIGHT,fruits, snakes):
                 return Direction.RIGHT
-            if not self.check_if_position_in_snake(pos,Direction.UP,fruits, snakes):
+            if not self.check_if_position_in_snake(pos,Direction.UP,fruits, snakes,pos_to_check):
                 return Direction.UP
             return Direction.DOWN
         else: 
             return direction
+    
+
+    def make_decision(self, board_state):
+
+        pos = super().allowed__body_position()
+        fruits = board_state["fruits"]
+        snakes = board_state["snakes"]
+        
+        # fruit_to_follow = fruits[0]
+ 
+        not_harmful_fruits = [fruit for fruit in fruits if fruit.kind not in FruitKind.harmful_fruits]
+              
+                
+        other_snakes = [snake for snake in snakes if snake.__class__  != Tehila]
+        if other_snakes == []:
+            not_harmful_fruits = [fruit for fruit in fruits if fruit.kind  in FruitKind.beneficial_fruits]
+        
+        fruit_to_follow =  not_harmful_fruits[0]
+        min_fruit_dis =  self.get_distance(pos[0], not_harmful_fruits[0].pos)
+        for snake in snakes:
+            for fruit in not_harmful_fruits:
+                snake_dis = self.get_distance(snake.get_body_position()[0], fruit.pos)
+                fruit_dis = self.get_distance(pos[0], fruit.pos)
+                if fruit_dis <= min_fruit_dis and (snake.__class__  == Tehila  or fruit_dis < snake_dis):
+                    min_fruit_dis = fruit_dis
+                    fruit_to_follow = fruit
+        
+
+
+
+        direction = self.allowed__change_direction(pos, fruit_to_follow, fruits, snakes, pos[0])
+        direction2 = self.get_direction_after_clear_way(direction, pos, fruits, snakes,pos[0])        
+        # if self.check_if_position_in_snake(pos,direction, fruits, snakes, pos_to_check=self.get_pos_to_check(direction2,pos[0])):
+        #     print ("WIll lose")
+        # direction2 = self.get_direction_after_clear_way(direction, pos, fruits, snakes,pos[0])
+        return direction2
+        
+
             
             
 
