f12d06b8c8270e8916a8e0d031073672334bde08
eliya188 <eliyatzanani188@gmail.com>
2022-11-02 17:19:49 +0200

    third

--- snakes_battle/snakes_ai/elitz_snake.py
+++ snakes_battle/snakes_ai/elitz_snake.py
@@ -10,39 +10,33 @@ class ElitzSnake(Snake):
 
     
     def make_decision(self, board_state):
-        distance = 100000
+        min_distance = 100000
         fruits = board_state["fruits"]
         pos = self.body_position
-        # for item in fruits:
-        #     if item.kind in FruitKind.harmful_fruits:
-        #         distance.append(pow(2, 31)-1)
-        #     else:
-        #         distance.append([abs(item.pos[0] - pos[0][0]) + abs(item.pos[1] - pos[0][1]), fruits.indexOf(item)])
-
 
         for item in fruits:
             dic = abs(item.pos[0] - pos[0][0]) + abs(item.pos[1] - pos[0][1])
-            # if dic < distance 
+            if dic < min_distance and item.kind not in FruitKind.harmful_fruits and item.kind in FruitKind.special_fruits:
+                the_fruit = item
+            elif dic < min_distance and item.kind not in FruitKind.harmful_fruits:
+                the_fruit = item
+            elif item.kind not in FruitKind.harmful_fruits:
+                the_fruit = item
             # to_get = distance.append(abs(item.pos[0] - pos[0][0]) + abs(item.pos[1] - pos[0][1]))
             # if item.kind  not in FruitKind.harmful_fruits and item.kind in FruitKind.special_fruits and to_get < item.lifespan:
             #     continue
-            if item.kind  in FruitKind.harmful_fruits:
-                continue
             # elif item.kind in FruitKind.special_fruits and :
             #     pass
-            else:
-                the_fruit = item
-                break
         # print(fruits[0].kind['name'])
         # print(the_fruit.pos , the_fruit.kind['name'])
         if pos[0][0] > the_fruit.pos[0]:
-            if self._direction == Direction.RIGHT:
+            if self.direction == Direction.RIGHT:
                 return Direction.UP
             else:
                 return Direction.LEFT
         
         if pos[0][0] < the_fruit.pos[0]:
-            if self._direction == Direction.LEFT:
+            if self.direction == Direction.LEFT:
                 return Direction.UP
             else:
                 return Direction.RIGHT
@@ -50,13 +44,13 @@ class ElitzSnake(Snake):
         if pos[0][0] == the_fruit.pos[0]:
 
             if pos[0][1] < the_fruit.pos[1]:
-                if self._direction == Direction.UP:
+                if self.direction == Direction.UP:
                     return Direction.RIGHT
                 else:
                     return Direction.DOWN
 
             if pos[0][1] > the_fruit.pos[1]:
-                if self._direction == Direction.DOWN:
+                if self.direction == Direction.DOWN:
                     return Direction.RIGHT
                 else:
                     return Direction.UP
