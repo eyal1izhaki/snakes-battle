1a1f09bf8f01e1afd24c36283c2d6c7de7a29dc1
yoav101 <yoavaharoni101@gmail.com>
2022-11-03 13:43:17 +0200

    Upgraded 3.0

--- snakes_battle/snakes_ai/yoav_snake.py
+++ snakes_battle/snakes_ai/yoav_snake.py
@@ -7,23 +7,24 @@ class YoavSnake(Snake):
 
         self.version = 1.0
         self.border_cells = borders_cells
-
+        self.directions = [0, -1], [0, 1], [1, 0], [-1, 0]
+    
     def is_harmful(self, fruits, dir):
-        pos = self.body_position
-        for item in fruits:
-            curr_fruit = item.kind["name"]
-            if curr_fruit == "BOMB" or curr_fruit == "SKULL":
-                if item.pos[0] == pos[0][0] + dir[0] and item.pos[1] == pos[0][1] + dir[1]:
+        x, y = dir
+        head = self.body_position[0]
+        for fruit in fruits:
+            if fruit.kind in FruitKind.harmful_fruits:
+                if fruit.pos[0] == head[0] + x and fruit.pos[1] == head[1] + y:
                     return True
         return False
 
     def is_snake(self, snakes, dir):
         x, y = dir
-        pos = self.body_position
+        head = self.body_position[0]
         for snake in snakes:
             curr_snake = snake.body_position
             for body_part in curr_snake:
-                if body_part[0] == pos[0][0] + x and body_part[1] == pos[0][1] + y:
+                if body_part[0] == head[0] + x and body_part[1] == head[1] + y:
                     return True
         return False
 
@@ -41,13 +42,15 @@ class YoavSnake(Snake):
         for idx, fruit in enumerate(fruits):
             if fruit.kind in FruitKind.harmful_fruits:
                 continue
+            elif fruit.kind in FruitKind.special_fruits:
+                return fruit
             elif fruits[idx+1].kind in FruitKind.special_fruits:
                 return fruits[idx+1]
             else:
                 return fruit
 
     def do_this_move(self, snakes):
-        up, down, right, left = [0, -1], [0, 1], [1, 0], [-1, 0]
+        up, down, right, left = self.directions
         if (not self.is_snake(snakes, up) and not self.is_border(up)):
             return Direction.UP
         if (not self.is_snake(snakes, down) and not self.is_border(down)):
@@ -57,10 +60,16 @@ class YoavSnake(Snake):
         if (not self.is_snake(snakes, left) and not self.is_border(left)):
             return Direction.LEFT
         return Direction.CONTINUE
-        
+
+    def no_problem_found(self, board_state, cmd):
+        fruits = board_state["fruits"]
+        snakes = board_state["snakes"]
+        if (self.is_harmful(fruits, cmd) or self.is_snake(snakes, cmd) or self.is_border(cmd)):
+            return False
+        return True
     
     def make_decision(self, board_state):
-        up, down, right, left = [0, -1], [0, 1], [1, 0], [-1, 0]
+        up, down, right, left = self.directions
         fruits = board_state["fruits"]
         snakes = board_state["snakes"]
         fruit = self.get_next_fruit(fruits)
@@ -68,44 +77,44 @@ class YoavSnake(Snake):
 
         if pos[0][0] > fruit.pos[0]:
             if self.direction == Direction.RIGHT:
-                if (not self.is_harmful(fruits, up) and not self.is_snake(snakes, up) and not self.is_border(up)):
+                if (self.no_problem_found(board_state, up)):
                     return Direction.UP
-                elif(not self.is_snake(snakes, down) and not self.is_border(down)):
+                elif(self.no_problem_found(board_state, down)):
                     return Direction.DOWN
             else:
-                if (not self.is_harmful(fruits, left) and not self.is_snake(snakes, left) and not self.is_border(left)):
+                if (self.no_problem_found(board_state, left)):
                     return Direction.LEFT
         
         if pos[0][0] < fruit.pos[0]:
             if self.direction == Direction.LEFT:
-                if (not self.is_harmful(fruits, up) and not self.is_snake(snakes, up) and not self.is_border(up)):
+                if (self.no_problem_found(board_state, up)):
                     return Direction.UP
-                elif(not self.is_snake(snakes, down) and not self.is_border(down)):
+                elif(self.no_problem_found(board_state, down)):
                     return Direction.DOWN
             else:
-                if (not self.is_harmful(fruits, right) and not self.is_snake(snakes, right) and not self.is_border(right)):
+                if (self.no_problem_found(board_state, right)):
                     return Direction.RIGHT
         
         if pos[0][0] == fruit.pos[0]:
 
             if pos[0][1] < fruit.pos[1]:
                 if self.direction == Direction.UP:
-                    if (not self.is_harmful(fruits, right) and not self.is_snake(snakes, right) and not self.is_border(right)):
+                    if (self.no_problem_found(board_state, right)):
                         return Direction.RIGHT
-                    elif(not self.is_snake(snakes, left) and not self.is_border(left)):
+                    elif(self.no_problem_found(board_state, left)):
                         return Direction.LEFT
                 else:
-                    if (not self.is_harmful(fruits, down) and not self.is_snake(snakes, down) and not self.is_border(down)):
+                    if (self.no_problem_found(board_state, down)):
                         return Direction.DOWN
 
             if pos[0][1] > fruit.pos[1]:
                 if self.direction == Direction.DOWN:
-                    if (not self.is_harmful(fruits, right) and not self.is_snake(snakes, right) and not self.is_border(right)):
+                    if (self.no_problem_found(board_state, right)):
                         return Direction.RIGHT
-                    elif(not self.is_snake(snakes, left) and not self.is_border(left)):
+                    elif(self.no_problem_found(board_state, left)):
                         return Direction.LEFT
                 else:
-                    if (not self.is_harmful(fruits, up) and not self.is_snake(snakes, up) and not self.is_border(up)):
+                    if (self.no_problem_found(board_state, up)):
                         return Direction.UP
             
         return self.do_this_move(snakes)
